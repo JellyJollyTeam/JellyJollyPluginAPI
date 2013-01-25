cb927651feb6783d78eb9c33cb057f9402e011f8
Ray <Ray@192.168.0.100>
2013-01-25 23:13:20 +0800

    更新协议头

--- src/main/java/cn/edu/seu/cose/jellyjolly/plugin/event/BlogPageEvent.java
+++ src/main/java/cn/edu/seu/cose/jellyjolly/plugin/event/BlogPageEvent.java
@@ -14,7 +14,6 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-
 package cn.edu.seu.cose.jellyjolly.plugin.event;
 
 import cn.edu.seu.cose.jellyjolly.model.BlogPage;
